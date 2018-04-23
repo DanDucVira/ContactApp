4bb973b57f9b40acae1f444d6c0d01feec76c317
Daniel Ducas-Viramontes <dducasviramontes@gmail.com>
2018-04-23 13:42:25 -0400

    Add files via upload

---
+++ ContactsList/ContactsList/Contacts.Designer.cs
@@ -0,0 +1,374 @@
+﻿namespace ContactsList
+{
+    partial class Contacts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblName = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCity = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.LblnameInput = new System.Windows.Forms.Label();
+            this.LbllastnameInput = new System.Windows.Forms.Label();
+            this.LblphoneInput = new System.Windows.Forms.Label();
+            this.Lblemailinput = new System.Windows.Forms.Label();
+            this.Lblcityinput = new System.Windows.Forms.Label();
+            this.LbladdressInput = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LblName
+            // 
+            this.LblName.AutoSize = true;
+            this.LblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblName.Location = new System.Drawing.Point(46, 112);
+            this.LblName.Name = "LblName";
+            this.LblName.Size = new System.Drawing.Size(52, 18);
+            this.LblName.TabIndex = 0;
+            this.LblName.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(146, 112);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(142, 20);
+            this.txtName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(146, 174);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(142, 20);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 176);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last Name:";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(146, 224);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(142, 20);
+            this.txtPhone.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Phone:";
+            // 
+            // txtCity
+            // 
+            this.txtCity.FormattingEnabled = true;
+            this.txtCity.Location = new System.Drawing.Point(149, 328);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(139, 21);
+            this.txtCity.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(47, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 18);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "City:";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(145, 382);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(142, 107);
+            this.txtAddress.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(45, 382);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 18);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Address:";
+            // 
+            // txtemail
+            // 
+            this.txtemail.Location = new System.Drawing.Point(145, 275);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(142, 20);
+            this.txtemail.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 275);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Email:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(670, 541);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.LbladdressInput);
+            this.panel1.Controls.Add(this.Lblcityinput);
+            this.panel1.Controls.Add(this.Lblemailinput);
+            this.panel1.Controls.Add(this.LblphoneInput);
+            this.panel1.Controls.Add(this.LbllastnameInput);
+            this.panel1.Controls.Add(this.LblnameInput);
+            this.panel1.Controls.Add(this.label11);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Location = new System.Drawing.Point(359, 100);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(423, 388);
+            this.panel1.TabIndex = 13;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(52, 11);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Name:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(52, 73);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(84, 18);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Last Name:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(52, 123);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 18);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Phone:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(52, 174);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 18);
+            this.label9.TabIndex = 16;
+            this.label9.Text = "Email:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(52, 228);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 18);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "City:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(52, 279);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(66, 18);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Address:";
+            // 
+            // LblnameInput
+            // 
+            this.LblnameInput.AutoSize = true;
+            this.LblnameInput.BackColor = System.Drawing.SystemColors.Highlight;
+            this.LblnameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblnameInput.Location = new System.Drawing.Point(199, 14);
+            this.LblnameInput.Name = "LblnameInput";
+            this.LblnameInput.Size = new System.Drawing.Size(0, 18);
+            this.LblnameInput.TabIndex = 19;
+            // 
+            // LbllastnameInput
+            // 
+            this.LbllastnameInput.AutoSize = true;
+            this.LbllastnameInput.BackColor = System.Drawing.SystemColors.Highlight;
+            this.LbllastnameInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbllastnameInput.Location = new System.Drawing.Point(199, 73);
+            this.LbllastnameInput.Name = "LbllastnameInput";
+            this.LbllastnameInput.Size = new System.Drawing.Size(0, 18);
+            this.LbllastnameInput.TabIndex = 20;
+            // 
+            // LblphoneInput
+            // 
+            this.LblphoneInput.AutoSize = true;
+            this.LblphoneInput.BackColor = System.Drawing.SystemColors.Highlight;
+            this.LblphoneInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblphoneInput.Location = new System.Drawing.Point(199, 126);
+            this.LblphoneInput.Name = "LblphoneInput";
+            this.LblphoneInput.Size = new System.Drawing.Size(0, 18);
+            this.LblphoneInput.TabIndex = 21;
+            // 
+            // Lblemailinput
+            // 
+            this.Lblemailinput.AutoSize = true;
+            this.Lblemailinput.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Lblemailinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblemailinput.Location = new System.Drawing.Point(199, 177);
+            this.Lblemailinput.Name = "Lblemailinput";
+            this.Lblemailinput.Size = new System.Drawing.Size(0, 18);
+            this.Lblemailinput.TabIndex = 22;
+            // 
+            // Lblcityinput
+            // 
+            this.Lblcityinput.AutoSize = true;
+            this.Lblcityinput.BackColor = System.Drawing.SystemColors.Highlight;
+            this.Lblcityinput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Lblcityinput.Location = new System.Drawing.Point(199, 231);
+            this.Lblcityinput.Name = "Lblcityinput";
+            this.Lblcityinput.Size = new System.Drawing.Size(0, 18);
+            this.Lblcityinput.TabIndex = 23;
+            // 
+            // LbladdressInput
+            // 
+            this.LbladdressInput.AutoSize = true;
+            this.LbladdressInput.BackColor = System.Drawing.SystemColors.Highlight;
+            this.LbladdressInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LbladdressInput.Location = new System.Drawing.Point(199, 282);
+            this.LbladdressInput.Name = "LbladdressInput";
+            this.LbladdressInput.Size = new System.Drawing.Size(0, 18);
+            this.LbladdressInput.TabIndex = 24;
+            // 
+            // Contacts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(801, 576);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.LblName);
+            this.Name = "Contacts";
+            this.Text = "Contacts";
+            this.Load += new System.EventHandler(this.Contacts_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox txtCity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label LbladdressInput;
+        private System.Windows.Forms.Label Lblcityinput;
+        private System.Windows.Forms.Label Lblemailinput;
+        private System.Windows.Forms.Label LblphoneInput;
+        private System.Windows.Forms.Label LbllastnameInput;
+        private System.Windows.Forms.Label LblnameInput;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ContactsList/ContactsList/Contacts.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactsList
+{
+    public partial class Contacts : Form
+    {
+        public Contacts()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            LblnameInput.Text = txtName.Text;
+            LbllastnameInput.Text = txtLastName.Text;
+            LblphoneInput.Text = txtPhone.Text;
+            Lblemailinput.Text = txtemail.Text;
+            Lblcityinput.Text = txtCity.Text;
+            LbladdressInput.Text = txtAddress.Text;
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Contacts_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
